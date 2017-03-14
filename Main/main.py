57b21699fe752f24881ee658a49c64d66c7f0dce
Ryan Berndt <rberndt@users.noreply.github.com>
2017-03-14 18:08:45 -0400

    ryan changes

--- Main/main.py
+++ Main/main.py
@@ -1,5 +1,15 @@
 import sys
 sys.path.insert(0, '../Engine/Logic')
 from FuzzyLogic import *
+sys.path.insert(0, '../Communication')
+from Server import *
 
 
+def Main():
+    Server = server()
+    while True:
+        Server.receive()
+        Server.send("RIGHT")
+     
+if __name__ == '__main__':
+    Main()
